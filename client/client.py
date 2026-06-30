f577c37e07b99ff954e2a8570b8a8fa97b3682f0
Aastha W <waastha4@gmail.com>
2026-07-01 03:23:33 +0400

    Add authentication and LIST command support

--- client/client.py
+++ client/client.py
@@ -1,17 +1,37 @@
 import socket
 
 HOST = "127.0.0.1"
-PORT = 5000
+PORT = 5000   # use whatever port your server uses
 
 client = socket.socket(socket.AF_INET,
                        socket.SOCK_STREAM)
 
 client.connect((HOST, PORT))
 
-client.send("LIST".encode())
+username = input("Username: ")
+password = input("Password: ")
 
-response = client.recv(4096).decode()
+login_command = f"LOGIN {username} {password}"
+
+client.send(login_command.encode())
+
+response = client.recv(1024).decode()
 
 print(response)
+
+if response == "LOGIN SUCCESS":
+
+    while True:
+
+        command = input("FTP> ")
+
+        client.send(command.encode())
+
+        response = client.recv(4096).decode()
+
+        print(response)
+
+        if command.upper() == "QUIT":
+            break
 
 client.close()
