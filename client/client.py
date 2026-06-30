48b54906ceaadd6a7fb772b6e8c2d6cd95d40cc3
Aastha W <waastha4@gmail.com>
2026-06-30 23:32:10 +0400

    Add client file

---
+++ client/client.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+client = socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM)
+
+client.connect((HOST, PORT))
+
+client.send("Hello Server".encode())
+
+response = client.recv(1024).decode()
+
+print(response)
+
+client.close()
