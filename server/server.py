6c85b63510bbae8051130fcbdff1d57941088c08
Aastha W <waastha4@gmail.com>
2026-07-01 03:40:13 +0400

    implement file download functionality

--- server/server.py
+++ server/server.py
@@ -18,6 +18,8 @@ def authenticate(username, password):
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 server.bind((HOST, PORT))
 server.listen()
 
@@ -41,6 +43,9 @@ while True:
 
         parts = command.split()
 
+        if len(parts) == 0:
+            continue
+
         if parts[0] == "LOGIN":
 
             username = parts[1]
@@ -55,7 +60,10 @@ while True:
 
             files = os.listdir("../storage")
 
-            response = "\n".join(files)
+            if len(files) == 0:
+                response = "Storage is empty"
+            else:
+                response = "\n".join(files)
 
             client_socket.send(response.encode())
 
@@ -80,9 +88,67 @@ while True:
 
             client_socket.send("UPLOAD SUCCESS".encode())
 
+        elif parts[0] == "DOWNLOAD":
+
+            filename = parts[1]
+
+            filepath = f"../storage/{filename}"
+
+            if not os.path.exists(filepath):
+
+                client_socket.send(
+                    "FILE_NOT_FOUND".encode()
+                )
+
+            else:
+
+                filesize = os.path.getsize(filepath)
+
+                client_socket.send(
+                    f"SIZE {filesize}".encode()
+                )
+
+                with open(filepath, "rb") as f:
+
+                    while True:
+
+                        chunk = f.read(1024)
+
+                        if not chunk:
+                            break
+
+                        client_socket.sendall(chunk)
+
+        elif parts[0] == "DELETE":
+
+            filename = parts[1]
+
+            filepath = f"../storage/{filename}"
+
+            if os.path.exists(filepath):
+
+                os.remove(filepath)
+
+                client_socket.send(
+                    "DELETE SUCCESS".encode()
+                )
+
+            else:
+
+                client_socket.send(
+                    "FILE NOT FOUND".encode()
+                )
+
         elif parts[0] == "QUIT":
 
             client_socket.send("Goodbye".encode())
+
             break
+
+        else:
+
+            client_socket.send(
+                "INVALID COMMAND".encode()
+            )
 
     client_socket.close()
